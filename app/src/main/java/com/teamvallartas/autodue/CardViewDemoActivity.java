9a0c056672fd95b2569fe6f16004f8f59cdafc36
Billy Andika <bandika@ucsd.edu>
2015-11-21 21:54:18 -0800

    Fix bug in color indicator and lighten card view

--- app/src/main/java/com/teamvallartas/autodue/CardViewDemoActivity.java
+++ app/src/main/java/com/teamvallartas/autodue/CardViewDemoActivity.java
@@ -2,6 +2,7 @@ package com.teamvallartas.autodue;
 
 import android.annotation.TargetApi;
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.widget.CardView;
@@ -9,6 +10,7 @@ import android.text.format.DateUtils;
 import android.transition.ChangeTransform;
 import android.view.View;
 import android.view.Window;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.grokkingandroid.samplesapp.samples.recyclerviewdemo.R;
@@ -39,6 +41,7 @@ public class CardViewDemoActivity extends Activity {
         TextView dateTime = (TextView) cardView.findViewById(R.id.txt_date_time);
         TextView labelDuration = (TextView) cardView.findViewById(R.id.txt_duration);
         TextView labelPriority = (TextView) cardView.findViewById(R.id.txt_priority);
+        ImageView colorIndicator = (ImageView) cardView.findViewById(R.id.color_indicator);
 
         // Set task title
         label.setText(model.label);
@@ -51,6 +54,21 @@ public class CardViewDemoActivity extends Activity {
 
         long millis = System.currentTimeMillis();
         int hoursLeft = (int) ((model.dateTime.getTime() - millis)/(1000*60*60));
+
+        int color = Color.parseColor("#000000");
+        int hoursInDay = 24;
+
+        if (hoursLeft <= 0) {}
+        else if(hoursLeft < hoursInDay) {color = Color.parseColor("#f04141");}
+        else if (hoursLeft < hoursInDay*2) {color = Color.parseColor("#eb553b");}
+        else if (hoursLeft < hoursInDay*3) {color = Color.parseColor("#eb6336");}
+        else if (hoursLeft < hoursInDay*4) {color = Color.parseColor("#fc7e3f");}
+        else if (hoursLeft < hoursInDay*5) {color = Color.parseColor("#fa9837");}
+        else if (hoursLeft < hoursInDay*6) {color = Color.parseColor("#faad32");}
+        else {color = Color.parseColor("#ffdb38");}
+
+        colorIndicator.setColorFilter(color);
+
         dateTime.setText("Due " + dateStr + " (" + hoursLeft + " hours left)");
         labelDuration.setText("Time needed: " + model.duration/3600000 + " hours");
 
